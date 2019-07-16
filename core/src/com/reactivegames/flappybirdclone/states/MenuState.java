9bfbd601e84b29399fd0313690252c6d168c3b21
RSapeliuk <r.sapeliuk@gmail.com>
2019-07-16 16:09:03 +0300

    Deleted useless files

--- core/src/com/reactivegames/flappybirdclone/states/MenuState.java
+++ core/src/com/reactivegames/flappybirdclone/states/MenuState.java
@@ -12,9 +12,10 @@ public class MenuState extends State {
     private Texture background;
     private Texture playBtn;
     private Music menuMusic;
+
     public MenuState(GameStateManager gsm) {
         super(gsm);
-        camera.setToOrtho(false,FlappyBirds.WIDTH/2,FlappyBirds.HEIGHT/2);
+        camera.setToOrtho(false, FlappyBirds.WIDTH / 2, FlappyBirds.HEIGHT / 2);
         this.background = new Texture("sprites/background-day.png");
         this.playBtn = new Texture("sprites/message.png");
         menuMusic = Gdx.audio.newMusic(Gdx.files.internal("audio/menuMusic.mp3"));
@@ -24,7 +25,7 @@ public class MenuState extends State {
 
     @Override
     protected void handledInput() {
-        if(Gdx.input.isTouched()|| Gdx.input.isKeyPressed(Input.Keys.ANY_KEY)){
+        if (Gdx.input.isTouched() || Gdx.input.isKeyPressed(Input.Keys.ANY_KEY)) {
             gsm.set(new PlayState(gsm));
         }
 
